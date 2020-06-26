72274c5e1461e512cc712bbaf06d6d3c9b4f158b
stvansolano <stvansolano@outlook.com>
2020-06-25 20:45:12 -0600

    Sesion-01 completa

---
+++ material/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft;
+
+namespace ConsoleApp
+{
+    public class Person
+    {
+        public int Id { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
